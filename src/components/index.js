84414defb7005be28151fb027f7b1cf3dc857992
Nagujja Mariam <mirembemariam3@gmail.com>
2023-07-04 12:56:01 +0300

    created the comonnets needed and added aroute of each in app.js

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Navbar} from './Navbar/Navbar.jsx'
+export {default as Feed} from './Feed/Feed.jsx'
+export {default as ChannelDetail} from './ChannelDetail/ChannelDetail'
+export {default as SearchFeed} from './SearchFeed/SearchFeed'
+export {default as VideoDetail} from './VideoDetail/VideoDetail'
